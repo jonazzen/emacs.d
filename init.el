8bfbb0b09691be10663452de60a5e97579998af8
Jonas Emil Sommer <jonazzen@icloud.com>
2021-04-11 10:21:22 +0200

    /home/jonazzen/.emacs.d/init.el: 1618129282

--- init.el
+++ init.el
@@ -28,7 +28,7 @@
 ;; along with GNU Emacs; see the file COPYING.  If not, write to the
 ;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
 ;; Boston, MA 02110-1301, USA.
-;;test
+
 ;;; Code:
 
 (let ((minver "26.3"))
